da8b9e261b8ec759e0257bf2b0d9d9c99a566724
jaasritha07 <149222860+jaasritha07@users.noreply.github.com>
2024-09-06 14:09:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,8 @@ function getWeather() {
     const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
     const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
 
-    fetch(https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key})
+    fetch(https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+)
         .then(response => response.json())
         .then(data => {
             displayWeather(data);
@@ -20,7 +21,7 @@ function getWeather() {
             alert('Error fetching current weather data. Please try again.');
         });
 
-    fetch(https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key})
+    fetch(https://pro.openweathermap.org/data/2.5/forecast/hourly?lat={lat}&lon={lon}&appid={API key})
         .then(response => response.json())
         .then(data => {
             displayHourlyForecast(data.list);
